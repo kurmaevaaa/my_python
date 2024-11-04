c05018bbe60d9c4a819cec94905b1ea5b7b6d19f
kurmaevaaa <gerda7474@gmail.com>
2024-11-04 23:53:40 +0300

    Update Поиск общих участников.py

--- Лабораторная работа 3/Поиск общих участников.py
+++ Лабораторная работа 3/Поиск общих участников.py
@@ -1,10 +1,10 @@
 # TODO Напишите функцию find_common_participants
 def find_common_participants(first, second, arg = ','):
-    list1 = first.split(arg)
+    list1 = first.split(arg) #разделяю строки по заданному разделителю
     list2 = second.split(arg)
-    allpart = list(set(list1).intersection(list2))
-    allpart.sort()
-    return allpart
+    allpart = list(set(list1).intersection(list2)) #нахожу пересечение участников из двух групп
+    allpart.sort() #сортирую список
+    return allpart #возвращаю отсортированный список
 participants_first_group = "Иванов|Петров|Сидоров"
 participants_second_group = "Петров|Сидоров|Смирнов"
 
